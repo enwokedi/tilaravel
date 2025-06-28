78e4964d27df757f3611b2636a47eb6ed72414b4
Emmanuel Nwokedi <emmanuel.nwokedi@gmail.com>
2025-06-28 14:47:41 +0200

    added drop down right submenu

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/welcome', function () {return view('welcome');});
+
 # Home
 #------
 Route::get('/', function () {return view('home/index');});
